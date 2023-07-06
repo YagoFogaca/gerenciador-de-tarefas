03b7a83cdcfebda55f36cd249560ba82ce39cf6f
yagoFogaca <yagofogaca20@gmail.com>
2023-07-06 14:40:55 -0300

    feat: implementação da funcionalidade de criar tarefas para a semana toda

--- task_manager/app/Http/Controllers/TaskController.php
+++ task_manager/app/Http/Controllers/TaskController.php
@@ -20,15 +20,63 @@ class TaskController extends Controller
 
     public function createTask(Request $req)
     {
+        $days = [
+            0 => [
+                'hora' => '',
+                'dia' => 'seg',
+                'check' => false,
+                'tarefa' => ''
+            ],
+            1 => [
+                'hora' => '',
+                'dia' => 'ter',
+                'check' => false,
+                'tarefa' => ''
+            ],
+            2 => [
+                'hora' => '',
+                'dia' => 'qua',
+                'check' => false,
+                'tarefa' => ''
+            ],
+            3 => [
+                'hora' => '',
+                'dia' => 'qui',
+                'check' => false,
+                'tarefa' => ''
+            ],
+            4 => [
+                'hora' => '',
+                'dia' => 'sex',
+                'check' => false,
+                'tarefa' => ''
+            ],
+            5 => [
+                'hora' => '',
+                'dia' => 'sáb',
+                'check' => false,
+                'tarefa' => ''
+            ]
+        ];
+
         $new_task = [
             'hora' => $req->input('hora'),
-            'dia' =>  $req->input('dia'),
+            'dia' =>  strtolower($req->input('dia')),
             'check' => false,
             'tarefa' => $req->input('tarefa')
         ];
 
-        $task = new Task($new_task);
-        $task->save();
+        if ($new_task['dia'] === 'td') {
+            foreach ($days as $day => $value) {
+                $value['hora'] = $new_task['hora'];
+                $value['tarefa'] = $new_task['tarefa'];
+                $task = new Task($value);
+                $task->save();
+            }
+        } else {
+            $task = new Task($new_task);
+            $task->save();
+        }
         return redirect()->back();
     }
 
